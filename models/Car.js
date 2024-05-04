644c055f9bcdd42e232d43a50e0c501c6172c1b6
Waleed Khalid Kayani <waleedkayani14@yahoo.com>
2024-05-04 20:45:31 +0500

    feat: added registration number to car model

--- models/Car.js
+++ models/Car.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 const carSchema = new mongoose.Schema({
+  registrationNumber: {
+    type: String,
+    required: true,
+  },
   make: {
     type: String,
     required: true,
